7f035571f62221d394a1e67172e47e76b1c142d9
Patrick <Patrick>
2017-07-27 16:27:52 +0800

    Testing, again

---
+++ src/test_1_pmc/test_2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test_1_pmc;
+import acm.graphics.*;
+import acm.program.*;
+public class test_2 {
+    static int age = 10;
+    public static void main(String[] args) {
+        
+        System.out.println("Hi World.");
+        System.out.print("\tHi " + 21 + " Objects. ");
+        System.out.println("Hi " + 21);
+        System.out.println(21.57);
+        System.out.println(age);
+        System.out.println("Apple\n\n\n");
+        // TODO code application logic here
+    }
+}
---
+++ src/test_2/test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test_2;
+import acm.graphics.*;
+import acm.program.*;
+public class test {
+    static int age = 10;
+    public static void main(String[] args) {
+        
+        
+        System.out.println(age);
+        System.out.println("Apple\n\n\n");
+        // TODO code application logic here
+    }
+}
